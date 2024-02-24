0e5e96f064719fc42021aa35df8b9a3d7af2f2d2
Mahendra M <89565913+creazer-I@users.noreply.github.com>
2024-02-24 19:46:28 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,40 +1,56 @@
-function checkDifference(event) {
-    var x1 = event.clientX - canvas1.getBoundingClientRect().left;
-    var y1 = event.clientY - canvas1.getBoundingClientRect().top;
+window.onload = function() {
+    canvas1 = document.getElementById('imageCanvas1');
+    context1 = canvas1.getContext('2d');
 
-    var x2 = event.clientX - canvas2.getBoundingClientRect().left;
-    var y2 = event.clientY - canvas2.getBoundingClientRect().top;
+    canvas2 = document.getElementById('imageCanvas2');
+    context2 = canvas2.getContext('2d');
+
+    canvas1.addEventListener('click', function(event) {
+        checkDifference(event, canvas1, context1);
+        logCoordinates(event, canvas1);
+    });
+
+    canvas2.addEventListener('click', function(event) {
+        checkDifference(event, canvas2, context2);
+        logCoordinates(event, canvas2);
+    });
+
+    var img1 = new Image();
+    var img2 = new Image();
+
+    img1.onload = function() {
+        canvas1.width = img1.width;
+        canvas1.height = img1.height;
+        context1.drawImage(img1, 0, 0, img1.width, img1.height);
+    };
+
+    img2.onload = function() {
+        canvas2.width = img2.width;
+        canvas2.height = img2.height;
+        context2.drawImage(img2, 0, 0, img2.width, img2.height);
+    };
+
+    img1.src = './images/different-Image-collection1/OfficePuzzle--1.png';
+    img2.src = '/app/images/different-Image-collection1/OfficePuzzle--2.png';
+};
+
+function checkDifference(event, canvas, context) {
+    var x = event.clientX - canvas.getBoundingClientRect().left;
+    var y = event.clientY - canvas.getBoundingClientRect().top;
 
     var found = false;
 
     differences.forEach(function(difference, index) {
-        var dx1 = difference.x1 - x1;
-        var dy1 = difference.y1 - y1;
-        var distance1 = Math.sqrt(dx1 * dx1 + dy1 * dy1);
+        var dx = difference.x - x;
+        var dy = difference.y - y;
+        var distance = Math.sqrt(dx * dx + dy * dy);
 
-        var dx2 = difference.x2 - x2;
-        var dy2 = difference.y2 - y2;
-        var distance2 = Math.sqrt(dx2 * dx2 + dy2 * dy2);
-
-        if ((distance1 < 20) && !difference.found && !found) {
-            context1.beginPath();
-            context1.arc(difference.x1, difference.y1, 20, 0, 2 * Math.PI, false);
-            context1.lineWidth = 3;
-            context1.strokeStyle = 'green';
-            context1.stroke();
-
-            score += 100;
-            differenceCount++;
-            difference.found = true;
-            found = true;
-        }
-
-        if ((distance2 < 20) && !difference.found && !found) {
-            context2.beginPath();
-            context2.arc(difference.x2, difference.y2, 20, 0, 2 * Math.PI, false);
-            context2.lineWidth = 3;
-            context2.strokeStyle = 'green';
-            context2.stroke();
+        if (distance < 20 && !difference.found && !found) {
+            context.beginPath();
+            context.arc(difference.x, difference.y, 20, 0, 2 * Math.PI, false);
+            context.lineWidth = 3;
+            context.strokeStyle = 'green';
+            context.stroke();
 
             score += 100;
             differenceCount++;
@@ -56,4 +72,11 @@ function checkDifference(event) {
 
     document.getElementById('score').textContent = "Score: " + score;
     document.getElementById('differenceCount').textContent = "Differences found: " + differenceCount;
+}
+
+function logCoordinates(event, canvas) {
+    var x = event.clientX - canvas.getBoundingClientRect().left;
+    var y = event.clientY - canvas.getBoundingClientRect().top;
+
+    console.log("Clicked coordinates on Image: X = " + x + ", Y = " + y);
 }
